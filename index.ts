8d9e4bdb2b68697e649e2f7c893db477a68b51c0
Egehanozsoy <75164990+Egehanozsoy@users.noreply.github.com>
2022-10-23 05:26:58 +0300

    added real origin

--- index.ts
+++ index.ts
@@ -22,11 +22,10 @@ const app: Express = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "https://maestro-zeta.vercel.app",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
-
 app.use(cors(corsOptions));
 app.use("/api", apiRouter);
 
