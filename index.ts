35e9357b350853df2125ad98b524ebae787492c0
Egehanozsoy <75164990+Egehanozsoy@users.noreply.github.com>
2022-10-23 05:17:48 +0300

    remove cert

--- index.ts
+++ index.ts
@@ -8,10 +8,13 @@ import cors from "cors";
 import http from "http";
 import https from "https";
 import fs from "fs";
+import path from "path";
 
 var options = {
-  key: fs.readFileSync("./client-key.pem"),
-  cert: fs.readFileSync("./client-cert.pem"),
+  key: fs.readFileSync(path.join(__dirname, "./key.pem")),
+  cert: fs.readFileSync(path.join(__dirname, "./cert.pem")),
+  requestCert: false,
+  rejectUnauthorized: false
 };
 const port = process.env.PORT;
 
@@ -23,17 +26,16 @@ const corsOptions = {
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
+
 app.use(cors(corsOptions));
 app.use("/api", apiRouter);
 
+// // Create an HTTP service.
+// http.createServer(app).listen(port);
+// // Create an HTTPS service identical to the HTTP service.
+// // https.createServer(options, app).listen(port);
 
-
-// Create an HTTP service.
-http.createServer(app).listen(80);
-// Create an HTTPS service identical to the HTTP service.
-https.createServer(options, app).listen(port);
-
-// app.listen(port, () => {
-//   console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+});
 
