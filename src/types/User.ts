633dd44948c6f4ff338064f853ee0de2ac1fa5e3
Bahadır Kalaçay <bahadirkalacay@gmail.com>
2022-10-31 13:50:29 +0300

    user.ts

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,11 +1,14 @@
+import IEntity from "./Entity";
 // 1. Create an interface representing a document in MongoDB.
-interface IUser {
+interface IUser extends IEntity {
   uid: string;
   name: string;
+  gender: "male" | "female";
   phoneNumber?: string;
   email?: string;
   type: "athlete" | "brand";
   avatar?: string;
+  profileId: string;
+  favoriteAthletes?: string[];
 }
 export default IUser;
-
